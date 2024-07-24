a59c76c662eed52f0824f4a3749a66816de12630
harshita-orrizonte <harshita.garg@orrizonte.in>
2024-07-24 15:21:24 +0530

    chng

--- ep-frontend-app/src/components/common/Footer/Footer.tsx
+++ ep-frontend-app/src/components/common/Footer/Footer.tsx
@@ -3,11 +3,12 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <div className="footer">
-      <div className="row">
-        <div className="col-12 col-md-7 col-lg-8">
-          <img src="/assets/images/Footer/starInfluence.svg" alt="icon" />
-          <div className="rounded-full bg-slate-100 px-2 py-1 text-xs font-semibold text-slate-700">
-          “Appreciating the talent of people with a special spark
+      <div className="flex flex-wrap">
+        <div className="w-full md:w-7/12 lg:w-8/12">
+          <img src="../../assets/images/Footer/starInfluence.svg" alt="icon" className="width:10rem"
+       />
+          <div className="footer-label font-medium mt-5 px-4">
+            “Appreciating the talent of people with a special spark"
           </div>
         </div>
       </div>
--- ep-frontend-app/src/components/pages/Home/Home.tsx
+++ ep-frontend-app/src/components/pages/Home/Home.tsx
@@ -5,21 +5,21 @@ import Footer from "../../common/Footer/Footer";
 
 const Home: React.FC = () => {
 
-    const { log } = useLogger();
-    const { showToast } = useToast();
-    const { showLoader } = useLoader();
+    // const { log } = useLogger();
+    // const { showToast } = useToast();
+    // const { showLoader } = useLoader();
 
-    useEffect(() => {
-        try {
-            // const response = homeService.getHealthCheck();
-            // log('info', 'Home', response);
-            showToast('Success', 'Success', 'success');
-            showLoader();
-        } catch (error) {
-            log('error', 'Home', error);
-        }
+    // useEffect(() => {
+    //     try {
+    //         // const response = homeService.getHealthCheck();
+    //         // log('info', 'Home', response);
+    //         showToast('Success', 'Success', 'success');
+    //         showLoader();
+    //     } catch (error) {
+    //         log('error', 'Home', error);
+    //     }
 
-    }, []);
+    // }, []);
 
     return (
         <h1>
