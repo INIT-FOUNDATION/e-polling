5f87a2169aef0456badea1a6ec2019172395028f
Narsima Chilkuri <nchilkur@cisco.com>
2024-07-24 15:02:32 +0530

    changes

--- ep-frontend-app/src/components/common/Footer/Footer.tsx
+++ ep-frontend-app/src/components/common/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
       <div className="row">
         <div className="col-12 col-md-7 col-lg-8">
           <img src="/assets/images/Footer/starInfluence.svg" alt="icon" />
-          <div className="footer-label card-label fw-500 mt-5 underline">
+          <div className="rounded-full bg-slate-100 px-2 py-1 text-xs font-semibold text-slate-700">
           “Appreciating the talent of people with a special spark
           </div>
         </div>
